b5eb1985b7856fa01afc05fada4206724053f82c
Lucky Mishra <luckymishra4097gmail.com>
2026-05-08 13:48:16 +0530

    Fixed login route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
         {/* Protected Routes */}
